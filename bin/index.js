10a0cd9f3f13fb40e873ebbc73f5ca8b3b838432
watasi <jessoncheung@gmail.com>
2022-06-09 14:22:09 +0800

    更新

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 const program = require('commander')
+const fs = require('fs')
+const path = require('path')
 const inquirer = require('inquirer')
+const ora = require('ora')
 const { version } = require('../package.json')
 const utils = require('../utils/index')
 const { promptList } = require('../config/index')
@@ -17,13 +20,14 @@ program
 .command('create <projectName>')
 .description('create a project')
 .action(async (projectName, cmd) => {
-  green(`👽👽👽 欢迎使用@watasi/cli，轻松构建项目模版！🎉🎉🎉`)
+  green(`欢迎使用@watasi/cli，轻松构建项目模版！🎉🎉🎉`)
   // 检测是否存在同名文件夹
-  await checkDir(path.join(process.pwd(), projectName), projectName)
+  await checkDir(path.join(process.cwd(), projectName), projectName)
 
   inquirer.prompt(promptList).then(result => {
     const { url, gitName, name, val } = result.type
     green(`======开始构建${name}======`)
+    const spinner = ora(`开始构建${name}`).start()
     if(!url) {
       red(`暂不支持${name}...`)
       process.exit(1)
@@ -31,6 +35,7 @@ program
 
     exec('git clone ' + url, function(error, stdout, stderr) {
       if(error !== null) {
+        spinner.stop()
         red(`clone fail, ${error}`)
         return;
       }
@@ -38,13 +43,15 @@ program
       fs.rename(gitName, projectName, err => {
         if(err) {
           exec(`rm -rf ` + gitName, function(err, out) {});
+          spinner.stop()
           red(`${projectName}项目已存在`)
         } else {
-          green(`项目模版创建成！🎉🎉🎉`)
+          spinner.stop()
+          green(`√ 项目模版创建成！🎉🎉🎉`)
         }
       })
     })
   })
 })
 
-commander.parse(process.argv)
+program.parse(process.argv)
