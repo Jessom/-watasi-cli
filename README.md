d73207983557fcdb57c254cf4912512542eb18f9
watasi <jessoncheung@gmail.com>
2022-06-11 21:37:48 +0800

    添加游戏盒模版

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 ```bash
 npx @watasi/cli create my-app
 ```
-
-## source
+**注：脚手架中项目模版均是通过执行`git clone`命令下载至本地，需本地有git环境**<br />
+如需提交仓库，请**删除项目中的`.git`文件夹**
+## 模版地址
 - [uniapp2(基于uView2.x)](https://github.com/Jessom/uniuView)
--- config/index.js
+++ config/index.js
@@ -11,6 +11,14 @@ module.exports = {
         name: 'uniapp2(基于uView2.x)',
         val: '基于uView2.x创建的uniapp模版'
       }
+    }, {
+      name: 'gameBox(游戏盒模版)',
+      value: {
+        url: 'git@github.com:Jessom/gameBox.git',
+        gitName: 'uniuView',
+        name: 'gameBox(游戏盒模版)',
+        val: '快速开发游戏盒'
+      }
     }]
   }]
 }
